edc31d04eeca24982d3f58221a5888ab28ee9a46
useronuralp <61450895+useronuralp@users.noreply.github.com>
2020-03-23 12:33:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ConsoleApp1
+{
+
+    public class Metric_1
+    {
+        public static int Calculate(Microsoft.CodeAnalysis.SyntaxNode root) 
+        {
+            var random = root.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+            int ct_classified = 0;
+            foreach (var value in random)
+            {
+                var temp = value.ToFullString();
+                if (temp.Contains("class Program")) { continue; }
+                else
+                {
+                    var temp_tree = CSharpSyntaxTree.ParseText(temp);
+                    var temp_root = temp_tree.GetRoot();
+                    var random_2 = temp_root.DescendantNodes().OfType<BaseFieldDeclarationSyntax>().ToList();
+
+                    foreach (var tmp in random_2)
+                    {
+                        SyntaxFactory.Comment(tmp.ToFullString());
+                        var tmp_2 = tmp.ToFullString();
+                        if (tmp_2.Contains("Classified") || tmp_2.Contains("classified")) { ct_classified++; }
+                    }
+                }
+            }
+            return ct_classified;
+        }
+    }
+    class Program
+    {   
+        
+        static void Main()
+        {
+            string readContents;
+            using (StreamReader streamReader = new StreamReader("exampleC#.txt", Encoding.UTF8))
+            {
+                readContents = streamReader.ReadToEnd();
+            }
+            var tree = CSharpSyntaxTree.ParseText(readContents);
+            //Metriclerin içine passlenecek olan arkadaş burdaki root.
+            var root = tree.GetRoot();
+            //Alttaki satırdaki gibi de metricleri çağırıp içlerindeki 'Calculate' ile ne yapıyorlarsa onları hesaplattıracağız.
+            //int printable = Metric_1.Calculate(root);
+        }
+    }
+}
